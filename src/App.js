7f18d07b9fc4c49fd386ad121c784d617642b5a0
Jorge Gomez Opazo <jorge.gomezo@MacBook-Pro-de-ingycom-3.local>
2022-08-04 15:38:23 -0400

    prompt

--- src/App.js
+++ src/App.js
@@ -2,30 +2,32 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import PwaInstallPopupIOS from 'react-pwa-install-ios';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Prueba PWA React
-        </p>
-      </header>
-      <PwaInstallPopupIOS>
-        <div
-          delay={0}
-          style={{
-            padding: '15px 30px',
-            backgroundColor: 'blue',
-            color: 'white',
-            textAlign: 'center',
-          }}
-        >
-          Hola. Instala por aca tu APP
-        </div>
-      </PwaInstallPopupIOS>
-    </div>
-
+    <BrowserRouter basename='/my-pwa'>
+      {<div className="App">
+        <header className="App-header">
+          <p>
+            Prueba PWA React
+          </p>
+        </header>
+        <PwaInstallPopupIOS>
+          <div
+            delay={0}
+            style={{
+              padding: '15px 30px',
+              backgroundColor: 'blue',
+              color: 'white',
+              textAlign: 'center',
+            }}
+          >
+            Hola. Instala por aca tu APP
+          </div>
+        </PwaInstallPopupIOS>
+      </div>}
+    </BrowserRouter>
   );
 }
 
